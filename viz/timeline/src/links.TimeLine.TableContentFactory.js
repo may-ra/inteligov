6c636ca7deb4fb34534cd77db814e67a30ac1d4d
Sier Violencia <sierisimo@gmail.com>
2013-05-07 17:12:47 -0500

    Modified the TableContentFactory to work with div instead of table

--- viz/timeline/src/links.TimeLine.TableContentFactory.js
+++ viz/timeline/src/links.TimeLine.TableContentFactory.js
@@ -3,28 +3,29 @@
 	links.Timeline.TableContentFactory.prototype = new links.Timeline.ContentFactory(type);
 
 	links.Timeline.TableContentFactory.prototype.get = function(data) {
+	
 		var borderColor = data.color.toString(),
 		$table = $(
-			"<table>",
+			"<div>",
 			{
-				css:{width:"155px", height:"100%", "border-collapse":"collapse"},
+				css:{width:"120px",height:"100%", "border-collapse":"collapse"},
 				"class":"timeline-event-detail"
 			}
-		).attr("cellspacing",0).attr("cellpadding",0),
+		),
 		tdStyle = {"text-align":"center",height:"32px",padding:0},
 		$titleTR = 
-			$("<tr>").append(
+			$("<div>",{css:{"display":"block"}}).append(
 				[ data.imgB64 ?
 					$(
-						"<td>",
+						"<div>",
 						{
-							css:$.extend({"font-size":"10px"},tdStyle)
+							css:$.extend({"font-size":"10px","display":"inline"},tdStyle)
 						}
 					).append(
 						$(
 							"<div>",
 							{
-								css:{"border-style":"solid","border-width":"1px"},
+								css:{"border-style":"solid","border-width":"1px","float":"left"},
 								"class":["timeline-event-detail","timeline-event-detail-image"].join(" ")
 							}
 						).append(
@@ -38,7 +39,7 @@
 						)
 					): null,
 					$(
-						"<td>",
+						"<div>",
 						{
 							css:{"font-size":"10px",width:"120px","text-align":"left"},
 						}
@@ -47,7 +48,7 @@
 								[$(
 								"<div>",
 								{
-									css:{"font-size":"9px",width:"120px","text-align":"left",padding:"0 0 0 5px"},
+									css:{"font-size":"9px",width:"120px","text-align":"left",padding:"0 0 0 5px","display":"inline"},
 									text:data.label,
 									"class":"timeline-event-detail-label"
 								}
@@ -55,7 +56,7 @@
 							$(
 								"<div>",
 								{
-									css:{"border-style":"solid","border-width":"1px 1px 1px 0",padding:"2px 0 0 6px",height:"20px"},
+									css:{"border-style":"solid","border-width":"1px 1px 1px 0",padding:"2px 0 0 6px",height:data.imgB64?"20px":"12px"},
 									text:data.title,
 									"class":"timeline-event-detail timeline-event-detail-title"
 								}
@@ -64,7 +65,7 @@
 						$(
 							"<div>",
 							{
-								css:{"border-style":"solid","border-width":"1px 1px 1px 1px",height:"20px",padding:"2px 0 0 6px","margin-top":data.img?"10px":"2px","border-collapse":"collapse"},
+								css:{"border-style":"solid","border-width":"1px 1px 1px 1px",height:data.imgB64?"16px":"12px",padding:"2px 0 2px 6px","margin-top":"2px","border-collapse":"collapse"},
 								text:data.title,
 								"class":"timeline-event-detail timeline-event-detail-title"
 							}
@@ -73,23 +74,29 @@
 				]
 			),
 			$desc = data.desc ? 
-				$("<tr>").append(
+				$("<div>").append(
 					$(
-						"<td>",
+						"<div>",
 						{
-							css:$.extend({"font-size":"10px"},tdStyle)
+							css:{"font-size":"10px",height:"100%",padding:0}
 						}
 					).append(
 						$(
 							"<div>",
 							{
-								css:{"margin-left":"16px","border-style":"solid","border-width":"0 1px 1px 1px",height:"100%"},							
 								"class":"timeline-event-detail timeline-event-detail-description",
-								"rowspan":"2",
-								text: data.desc
+								css:{"margin-left":"12px","border-style":"solid","border-width":"0 1px 1px 1px",height:"100%","word-wrap":"break-word"},							
 							}
+						).append(
+							$(
+								"<p>",
+								{	
+									css:{"white-space": "pre-wrap","word-wrap":"break-word",display:"inline",height:"100%"},
+									text:data.desc
+								}
+							)
 						)
-					).attr("colSpan",2)
+					)
 				) : null;
 
 		fragment = document.createDocumentFragment();
--- viz/timeline/timeline.js
+++ viz/timeline/timeline.js
@@ -6447,28 +6447,29 @@ links.Timeline.ContentGenerator.prototype.setFactory = function(factory){
 	links.Timeline.TableContentFactory.prototype = new links.Timeline.ContentFactory(type);
 
 	links.Timeline.TableContentFactory.prototype.get = function(data) {
+	
 		var borderColor = data.color.toString(),
 		$table = $(
-			"<table>",
+			"<div>",
 			{
-				css:{width:"155px", height:"100%", "border-collapse":"collapse"},
+				css:{width:"120px",height:"100%", "border-collapse":"collapse"},
 				"class":"timeline-event-detail"
 			}
-		).attr("cellspacing",0).attr("cellpadding",0),
+		),
 		tdStyle = {"text-align":"center",height:"32px",padding:0},
 		$titleTR = 
-			$("<tr>").append(
+			$("<div>",{css:{"display":"block"}}).append(
 				[ data.imgB64 ?
 					$(
-						"<td>",
+						"<div>",
 						{
-							css:$.extend({"font-size":"10px"},tdStyle)
+							css:$.extend({"font-size":"10px","display":"inline"},tdStyle)
 						}
 					).append(
 						$(
 							"<div>",
 							{
-								css:{"border-style":"solid","border-width":"1px"},
+								css:{"border-style":"solid","border-width":"1px","float":"left"},
 								"class":["timeline-event-detail","timeline-event-detail-image"].join(" ")
 							}
 						).append(
@@ -6482,7 +6483,7 @@ links.Timeline.ContentGenerator.prototype.setFactory = function(factory){
 						)
 					): null,
 					$(
-						"<td>",
+						"<div>",
 						{
 							css:{"font-size":"10px",width:"120px","text-align":"left"},
 						}
@@ -6491,7 +6492,7 @@ links.Timeline.ContentGenerator.prototype.setFactory = function(factory){
 								[$(
 								"<div>",
 								{
-									css:{"font-size":"9px",width:"120px","text-align":"left",padding:"0 0 0 5px"},
+									css:{"font-size":"9px",width:"120px","text-align":"left",padding:"0 0 0 5px","display":"inline"},
 									text:data.label,
 									"class":"timeline-event-detail-label"
 								}
@@ -6499,7 +6500,7 @@ links.Timeline.ContentGenerator.prototype.setFactory = function(factory){
 							$(
 								"<div>",
 								{
-									css:{"border-style":"solid","border-width":"1px 1px 1px 0",padding:"2px 0 0 6px",height:"20px"},
+									css:{"border-style":"solid","border-width":"1px 1px 1px 0",padding:"2px 0 0 6px",height:data.imgB64?"20px":"12px"},
 									text:data.title,
 									"class":"timeline-event-detail timeline-event-detail-title"
 								}
@@ -6508,7 +6509,7 @@ links.Timeline.ContentGenerator.prototype.setFactory = function(factory){
 						$(
 							"<div>",
 							{
-								css:{"border-style":"solid","border-width":"1px 1px 1px 1px",height:"20px",padding:"2px 0 0 6px","margin-top":data.img?"10px":"2px","border-collapse":"collapse"},
+								css:{"border-style":"solid","border-width":"1px 1px 1px 1px",height:data.imgB64?"16px":"12px",padding:"2px 0 2px 6px","margin-top":"2px","border-collapse":"collapse"},
 								text:data.title,
 								"class":"timeline-event-detail timeline-event-detail-title"
 							}
@@ -6517,23 +6518,29 @@ links.Timeline.ContentGenerator.prototype.setFactory = function(factory){
 				]
 			),
 			$desc = data.desc ? 
-				$("<tr>").append(
+				$("<div>").append(
 					$(
-						"<td>",
+						"<div>",
 						{
-							css:$.extend({"font-size":"10px"},tdStyle)
+							css:{"font-size":"10px",height:"100%",padding:0}
 						}
 					).append(
 						$(
 							"<div>",
 							{
-								css:{"margin-left":"16px","border-style":"solid","border-width":"0 1px 1px 1px",height:"100%"},							
 								"class":"timeline-event-detail timeline-event-detail-description",
-								"rowspan":"2",
-								text: data.desc
+								css:{"margin-left":"12px","border-style":"solid","border-width":"0 1px 1px 1px",height:"100%","word-wrap":"break-word"},							
 							}
+						).append(
+							$(
+								"<p>",
+								{	
+									css:{"white-space": "pre-wrap","word-wrap":"break-word",display:"inline",height:"100%"},
+									text:data.desc
+								}
+							)
 						)
-					).attr("colSpan",2)
+					)
 				) : null;
 
 		fragment = document.createDocumentFragment();
